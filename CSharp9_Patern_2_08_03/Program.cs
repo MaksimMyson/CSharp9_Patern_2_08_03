9fd94099823b3e3c0c06207ac803dd0e8ca9cc9f
MaksimMyson <maksimmyson33@gmail.com>
2024-03-15 20:09:38 +0200

    adapter was done

--- CSharp9_Patern_2_08_03/Program.cs
+++ CSharp9_Patern_2_08_03/Program.cs
@@ -1,10 +1,47 @@
 ﻿namespace CSharp9_Patern_2_08_03
 {
-    internal class Program
+    interface ITarget
+    {
+        void Request();
+    }
+
+    
+    class Adaptee
+    {
+        public void SpecificRequest()
+        {
+            Console.WriteLine("Adaptee's specific request");
+        }
+    }
+
+    
+    class Adapter : ITarget
+    {
+        private readonly Adaptee adaptee;
+
+        public Adapter(Adaptee adaptee)
+        {
+            this.adaptee = adaptee;
+        }
+
+        public void Request()
+        {
+            adaptee.SpecificRequest();
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            Adaptee adaptee = new Adaptee();
+
+            // Створення адаптера
+            ITarget adapter = new Adapter(adaptee);
+
+            
+            adapter.Request();
         }
     }
 }
