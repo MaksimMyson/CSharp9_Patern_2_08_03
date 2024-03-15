09891b8078bb54f6e0327589d57c563dfb44909c
MaksimMyson <maksimmyson33@gmail.com>
2024-03-15 20:12:54 +0200

    bridge was done

--- CSharp9_Patern_2_08_03/Program.cs
+++ CSharp9_Patern_2_08_03/Program.cs
@@ -1,10 +1,71 @@
 ﻿namespace CSharp9_Patern_2_08_03
 {
-    internal class Program
+    abstract class Implementor
+    {
+        public abstract void OperationImp();
+    }
+
+    
+    class ConcreteImplementorA : Implementor
+    {
+        public override void OperationImp()
+        {
+            Console.WriteLine("ConcreteImplementorA operation");
+        }
+    }
+
+    
+    class ConcreteImplementorB : Implementor
+    {
+        public override void OperationImp()
+        {
+            Console.WriteLine("ConcreteImplementorB operation");
+        }
+    }
+
+   
+    class Abstraction
+    {
+        protected Implementor implementor;
+
+        public Implementor Implementor
+        {
+            set { implementor = value; }
+        }
+
+        public virtual void Operation()
+        {
+            implementor.OperationImp();
+        }
+    }
+
+    
+    class RefinedAbstraction : Abstraction
+    {
+        public override void Operation()
+        {
+            implementor.OperationImp();
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            Implementor implementorA = new ConcreteImplementorA();
+            Implementor implementorB = new ConcreteImplementorB();
+
+            
+            Abstraction abstraction1 = new Abstraction();
+            abstraction1.Implementor = implementorA;
+
+            Abstraction abstraction2 = new RefinedAbstraction();
+            abstraction2.Implementor = implementorB;
+
+           
+            abstraction1.Operation();
+            abstraction2.Operation();
         }
     }
 }
